b3193938a59966d5d77943ee6008154ac286a216
KazbekMusaev <145585208+KazbekMusaev@users.noreply.github.com>
2024-05-29 17:10:21 +0300

    firstCommit

---
+++ SomeGame/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  SomeGame
+//
+//  Created by KazbekMusaev on 29.05.2024.
+//
+
+import SpriteKit
+import GameplayKit
+
+final class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}
